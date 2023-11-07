1a6e448911ce05e4568a3ad90f9e9e2202de4c22
saloni kumawat <545saloni@gmail.com>
2023-11-07 23:13:59 +0100

    children prop

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,12 +1,22 @@
-import AccordionItem from "./AccordionItem";
-import "../css/style.css";
+import AccordionItem from './AccordionItem';
+import '../css/style.css';
+import { useState } from 'react';
 
-function Accordion({data}) {
-    return(
-        <div className="accordion">
-            {data.map((element, i) => <AccordionItem title={element.title} text={element.text} num={i} key={element.title}/>)}
-        </div>
-    );
+function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
+  return (
+    <div className='accordion'>
+      {data.map((element, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setIsOpen}
+          title={element.title}
+          num={i}
+          key={element.title}
+        >{element.text}</AccordionItem>
+      ))}
+    </div>
+  );
 }
 
 export default Accordion;
